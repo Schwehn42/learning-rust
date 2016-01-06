3175180457681a4baa901415e9dd53d076df8e46
Jakob Schwehn <schwehn42@gmail.com>
2016-01-06 17:24:58 +0100

    random numbers

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,7 +1,15 @@
+extern crate rand; //random crate from cargo
+
 use std::io; //import io lib for input
+use rand::Rng; // import rand lib (from cargo) to gen rand nums
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("secret number: {}", secret_number);
+
     println!("Please input your guess");
 
     let mut guess = String::new(); //input string that can be modified (hence mut)
