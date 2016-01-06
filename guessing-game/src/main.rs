5e3790368b7c9dffe0dec7ee75bc825cd29006e9
Jakob Schwehn <schwehn42@gmail.com>
2016-01-06 17:11:10 +0100

    input
    makes the user input sth

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io; //import io lib for input
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess");
+
+    let mut guess = String::new(); //input string that can be modified (hence mut)
+
+    //read a line from the user.
+
+    io::stdin().read_line(&mut guess)
+        .ok()
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
