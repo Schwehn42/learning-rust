b53566bcca26075f549a0e6634bafc9ddefcdc39
Jakob Schwehn <schwehn42@gmail.com>
2016-01-06 17:37:01 +0100

    Loop until win

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -11,27 +11,33 @@ fn main() {
 
     println!("secret number: {}", secret_number);
 
-    println!("Please input your guess");
+    //loop this forever until win
+    loop {
+        println!("Please input your guess");
 
-    let mut guess = String::new(); //input string that can be modified (hence mut)
+        let mut guess = String::new(); //input string that can be modified (hence mut)
 
-    //read a line from the user.
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("Failed to read line");
-
-    //convert string to unsigned int 32bit.
-    //take away its mutability
-    let guess: u32 = guess.trim().parse()
+        //read a line from the user.
+        io::stdin().read_line(&mut guess)
             .ok()
-            .expect("Please type a number!");
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        //convert string to unsigned int 32bit.
+        //take away its mutability
+        let guess: u32 = guess.trim().parse()
+                .ok()
+                .expect("Please type a number!");
 
-    //compare guess with secret_number
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+
+        //compare guess with secret_number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break; //break loop
+            }
+        }
     }
 }
